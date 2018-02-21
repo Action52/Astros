0ab641d5dfd8c72dcf1e611bc1aee37879239dd6
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2018-02-21 11:38:00 -0600

    Cambios en server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path =  require('path');
+
+var PORT = 8080;
+
+var app=express()
+
+app.use('/bootstrap-4.0.0-dist', express.static(path.join(__dirname, './bootstrap-4.0.0-dist')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(PORT);
+
+console.log('Running on port ' + PORT);
